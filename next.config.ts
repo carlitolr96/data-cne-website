90237f56a656652007e20ebd02370f3d1b1be68e
Carlos Luis Lachapell Rivera <carlosluis9631@gmail.com>
2025-09-23 12:14:17 -0400

    Fix json and Next.config pipeline

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export",
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/i,
-  //     issuer: /\.[jt]sx?$/,
-  //     use: ["@svgr/webpack"],
-  //   });
-  //   return config;
-  // },
 };
 
 export default nextConfig;
