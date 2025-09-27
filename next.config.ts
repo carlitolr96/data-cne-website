28a91a4be03943027b4f6cbbab00ccfd53850b83
Carlos Luis Lachapell Rivera <clachapell@CNE.GOB.DO>
2025-09-26 20:41:52 -0400

    Mejoras en next.conf azure-yml y package

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export', // activa exportación estática
   images: {
-    unoptimized: true,
+    unoptimized: true, // para que Azure no falle con la optimización de imágenes
   },
-  output: "export",
 };
 
 export default nextConfig;
